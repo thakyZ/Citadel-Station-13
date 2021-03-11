5c9493a186b87c576648220a69e966c936e99b61
thakyZ <950594+thakyZ@users.noreply.github.com>
2021-03-11 07:31:09 -0800

    Updated

--- make-links.bat
+++ make-links.bat
@@ -2,6 +2,6 @@
 
 move /Y "%~dp0config" "%~dp0config-bak"
 
-mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\Citadel-Station-13\config" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\config"
-mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\Citadel-Station-13\cfg" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\cfg"
-mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\Citadel-Station-13\data" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\data"
+mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\run\config" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\config"
+mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\run\cfg" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\cfg"
+mklink /d "C:\Users\thaky\Development\Git\Games\Space Station 13\run\data" "C:\Users\thaky\Development\Git\Games\Space Station 13\tgstation-server\gamedata\data"
